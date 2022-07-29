43f9fc19442fe8405a796afe2a9406140a10cee5
bagus25dzikri06 <bagusdzikri@gmail.com>
2022-07-29 16:41:08 +0700

    Task 1 (Biodata)

---
+++ task1.js
@@ -0,0 +1,51 @@
+let biodata = {
+  name: 'Bagus Dzikri Hidayat',
+  age: 27,
+  hobbies: ['reading', 'listening to the music', 'browsing'],
+  isMarried: false,
+  schoolList: [
+    {
+      name: 'SMA Yadika 12 Limo',
+      yearIn: 2010,
+      yearOut: 2013,
+      major: 'Natural Science'
+    },
+    {
+      name: 'Institut Pertanian Bogor',
+      yearIn: 2013,
+      yearOut: 2016,
+      major: 'Computer Science'
+    },
+    {
+      name: 'Universitas Nasional',
+      yearIn: 2016,
+      yearOut: 2019,
+      major: 'Information Systems'
+    }
+  ],
+  skills: [
+    {
+      skillName: 'JavaScript',
+      level: 'advanced'
+    },
+    {
+      skillName: 'Express',
+      level: 'advanced'
+    },
+    {
+      skillName: 'React',
+      level: 'beginner'
+    },
+    {
+      skillName: 'Next',
+      level: 'beginner'
+    },
+    {
+      skillName: 'Postgres',
+      level: 'advanced'
+    },
+  ],
+  interestInCoding: true
+}
+
+console.log(biodata)
